e8ecbc54324d95b23026b59467074d6e3652b52b
Praveen <pkchmits@gmail.com>
2021-10-20 16:41:54 +0530

    Photo and Image Picker functionality done

--- app/src/main/java/com/example/imagepickerapp/MainActivity.java
+++ app/src/main/java/com/example/imagepickerapp/MainActivity.java
@@ -1,22 +1,144 @@
 package com.example.imagepickerapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.bumptech.glide.Glide;
 import com.example.imagepickerapp.databinding.ActivityMainBinding;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 
+        binding.selectFromGallaryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pickFromGallery();
+            }
+        });
+
+        registerForContextMenu(binding.takePhotoBtn);
+        binding.takePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.showContextMenu();
+            }
+        });
+
 
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Select image using");
+        menu.add(0, v.getId(), 0, "Take Photo");
+        menu.add(0, v.getId(), 0, "Select From Gallery");
+        menu.add(0, v.getId(), 0, "Search At PixelBay");
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if (item.getTitle() == "Take Photo") {
+            dispatchTakePictureIntent();
+           return true;
+        }else if(item.getTitle() == "Select From Gallery"){
+            pickFromGallery();
+            return true;
+        }else if(item.getTitle() == "Search At PixelBay"){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.imagepickerapp.fileProvider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 100);
+            }
+        }
+    }
+    private String currentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void pickFromGallery() {
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickIntent, 200);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+
+            Glide.with(this)
+                    .load(currentPhotoPath)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .into(binding.imageView);
+        }
+        if (requestCode == 200 && resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            Glide.with(this)
+                    .load(uri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .into(binding.imageView);
+        }
+    }
+
 }
